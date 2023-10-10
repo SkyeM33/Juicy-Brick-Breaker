e2ab8ee70599083f3109ef0177025d04496c65bb
SkyeM33 <142693429+SkyeM33@users.noreply.github.com>
2023-10-10 14:29:35 -0400

    Updated project

--- Wall/Wall.gd
+++ Wall/Wall.gd
@@ -14,6 +14,8 @@ func hit(_ball):
 	tween.tween_property(self, "scale", Vector2(1,1), 0.5)
 	$ColorRect.color = Color8(201,42,42)
 	tween.tween_property($ColorRect, "color", Color8(255,255,255), 0.5)
+	var sound = get_node("/root/Game/Wall_Sound")
+	sound.play()
 	var Camera = get_node_or_null("/root/Game/Camera2D")
 	if Camera != null:
 		Camera.add_trauma(3.0)
